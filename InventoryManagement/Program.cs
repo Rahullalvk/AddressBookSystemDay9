3ab42bc41152d48599bcba156f88f692a980c2f0
Rahullal <Rahullal432@gmail.com>
2023-05-09 12:35:00 +0530

    create a Contacts in Address Book with first and last names, address,city, state, zip, phone number and email

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace InventoryManagement 
+{
+    class program 
+    {
+        public static void Main(String[] args) 
+        {
+            string path = "C:\\Users\\USER LENOVO\\Documents\\LineComparisonDay8\\InventoryManagement\\InventoryManagement\\jsconfig1.json";
+            ReadTheData readTheData = new ReadTheData();
+            InventoryDetails data =readTheData.Read(path);
+
+            Console.WriteLine(data.typeOfRice.name);
+            Console.WriteLine(data.typeOfRice.weight);
+            Console.WriteLine(data.typeOfRice.price);
+
+        }
+    }
+}
